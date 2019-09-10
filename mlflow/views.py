5a6ef7e03b5986d869ccdeff13bbd073c2009406
Prashant <prashant.ambe@gmail.com>
2019-09-10 11:32:43 -0400

    load list of files in select box

--- mlflow/views.py
+++ mlflow/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+import os
+from os.path import isfile, join
 
 def home_view(request):
-    return render(request, 'home.html', {"visibility":"visible"})
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    datafiles_path = os.path.join(BASE_DIR, 'data')
+    filenames = [f for f in os.listdir(datafiles_path) if isfile(join(datafiles_path, f))]
+    context = {"visibility":"invisible", "filenames":filenames}
+    return render(request, 'home.html', context)
