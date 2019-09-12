f3f81ff9aeec9fdccb363780083bcfd693027179
Prashant <prashant.ambe@gmail.com>
2019-09-12 10:25:42 -0400

    Increased file selection width

--- mlflow/selenium_tests/test_home_view.py
+++ mlflow/selenium_tests/test_home_view.py
@@ -25,15 +25,15 @@ class HomeViewTemplateTestCase(StaticLiveServerTestCase):
         self.assertEquals(flow_container.get_attribute('class'), 'invisible')
 
     def test_file_selector_has_placeholder(self):
-        file_selector = self.browser.find_element_by_id('input_file')
+        file_selector = self.browser.find_element_by_name('input_file')
         default = Select(file_selector).all_selected_options[0].text
         self.assertEquals(default, 'Select data file ...')
 
     def test_file_selector_contains_datafile_names(self):
-        file_selector = self.browser.find_element_by_id('input_file')
+        file_selector = self.browser.find_element_by_name('input_file')
         filelist = Select(file_selector).options
         self.assertGreater(len(filelist), 5)
         self.assertTrue("ex1data1.txt" in [opt.text for opt in filelist])
 
-    
+ 
         
