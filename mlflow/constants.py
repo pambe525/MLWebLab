fccee7475e7aa929a58015076f8b85c21293075b
Prashant <prashant.ambe@gmail.com>
2019-10-22 08:38:37 -0400

    Fixed flashing message box during home view reload Changed css for file dropdown to be consistent accross browsers

--- mlflow/constants.py
+++ mlflow/constants.py
@@ -5,6 +5,7 @@ from django.conf import settings
 # Application constants
 FILE_SELECT_DEFAULT = "< Choose a file >"
 DATA_FILE_PATH = os.path.join(settings.BASE_DIR, "data")
+FILE_SELECT_WIDGET_CLASS = "custom-select bg-dark text-white h5 select-override"
 CONTROL_PANEL_WIDGET_CLASS = "card-label custom-select col"
 TRAINING_METHOD_CHOICES = [("Linear Regression", "Linear Regression")]
 TRAINING_RATIO_CHOICES = [(0.8, "80%")]
